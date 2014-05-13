161ea4cabc2b317ef5ccbb0b49b118b93c7e5284
Alumno <alumno@localhost.localdomain>
2014-05-13 08:36:43 -0600

    rectangulo

---
+++ rectangulo/src/com/me/mygdxgame/MyGdxGame.java
@@ -0,0 +1,89 @@
+package com.me.mygdxgame;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.GL10;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.Texture.TextureFilter;
+import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Image;
+
+public class MyGdxGame implements ApplicationListener {
+	private OrthographicCamera camera;
+	private SpriteBatch batch;
+	private Texture texture;
+	private Sprite sprite;
+	Stage stage;
+	Image image;
+	
+	@Override
+	public void create() {		
+		float w = Gdx.graphics.getWidth();
+		float h = Gdx.graphics.getHeight();
+		
+		camera = new OrthographicCamera(1, h/w);
+		batch = new SpriteBatch();
+		
+		texture = new Texture(Gdx.files.internal("/home/alumno/new/rectangulo.png"));
+		texture.setFilter(TextureFilter.Linear, TextureFilter.Linear);
+		
+		TextureRegion region = new TextureRegion(texture, 0, 0, 512, 275);
+		
+		sprite = new Sprite(region);
+		sprite.setSize(0.9f, 0.9f * sprite.getHeight() / sprite.getWidth());
+		sprite.setOrigin(sprite.getWidth()/2, sprite.getHeight()/2);
+		sprite.setPosition(-sprite.getWidth()/2, -sprite.getHeight()/2);
+		
+	
+		stage = new Stage();
+		image = new Image(region);
+		
+	//image.setRotation(45);
+		// image.setX(50);
+		//image.getRight();
+		stage.addActor(image);
+	}
+
+	@Override
+	public void dispose() {
+		batch.dispose();
+		texture.dispose();
+	}
+
+	@Override
+	public void render() {		
+		Gdx.gl.glClearColor(1, 1, 1, 1);
+		Gdx.gl.glClear(GL10.GL_COLOR_BUFFER_BIT);
+		
+		batch.setProjectionMatrix(camera.combined);
+		batch.begin();
+		sprite.draw(batch);
+		batch.end();
+		
+	image.setRotation(image.getRotation()-1);
+	//image.setX(image.getX()+5);
+		//if(image.getX()>700)
+			//image.setX(-500);
+		
+			
+		stage.draw();
+	
+	}
+
+	@Override
+	public void resize(int width, int height) {
+	}
+
+	@Override
+	public void pause() {
+	}
+
+	@Override
+	public void resume() {
+	}
+}
+
